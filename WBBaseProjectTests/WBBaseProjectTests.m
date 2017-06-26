43a12d3dc13543372b6b9ba27de2e9140f771088
wubin123 <wubin@wubindemacbook-pro.local>
2017-06-26 15:38:58 +0800

    Initial Commit

---
+++ WBBaseProjectTests/WBBaseProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  WBBaseProjectTests.m
+//  WBBaseProjectTests
+//
+//  Created by 吴斌 on 2017/6/26.
+//  Copyright © 2017年 吴斌. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WBBaseProjectTests : XCTestCase
+
+@end
+
+@implementation WBBaseProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
